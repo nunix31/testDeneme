b45996cb0514e982fde1fd0d7373002df204df9a
nur yurtseven <karakosenurullah@gmail.com>
2023-03-15 13:22:31 +0300

    first commit

---
+++ src/test/java/Nur.java
@@ -0,0 +1,15 @@
+public class Nur {
+    public static void main(String[] args) {
+
+        System.out.println("deneme123");
+
+        /*
+        git init
+        git add README.md
+        git commit -m "first commit"
+        git branch -M main
+        git remote add origin https://github.com/karakosenur/testDeneme.git
+        git push -u origin main
+         */
+    }
+}
